434afd4e21b4850e7f372ffbc259d9b248036960
Michael Bigelow <procureeminence@gmail.com>
2020-04-23 18:39:40 -0400

    feat: Add artist search functionality

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def search_venues():
 	"count": len(venueList),
 	"data": data
 	}
-	
+
 	return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
@@ -337,17 +337,23 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-	# TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-	# seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-	# search for "band" should return "The Wild Sax Band".
+	searchString = request.form['search_term']
+	artistList = Artist.query.filter(Artist.name.ilike(f'%{searchString}%')).all()
+	data = []
+
+	for artist in artistList:
+
+		data.append({
+			"id": artist.id,
+			"name": artist.name,
+			"num_upcoming_shows": 0
+		})
+
 	response={
-	"count": 1,
-	"data": [{
-		"id": 4,
-		"name": "Guns N Petals",
-		"num_upcoming_shows": 0,
-	}]
+	"count": len(artistList),
+	"data": data
 	}
+
 	return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
